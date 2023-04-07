1d233532e2edc9b1b3127328ef2d9c9510935c86
David Knight <daveknight2000@gmail.com>
2023-04-07 08:41:38 +0100

    Update for v3 manifest. Prevent some errors.

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,16 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
   const targetUrl = 'https://opensea.io/collection/blockydoge';
   chrome.tabs.create({ url: targetUrl });
+});
+
+// Listen for messages from the content script
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'keepAlive') {
+    // Keep the service worker alive by resetting a timer
+    clearTimeout(self.keepAliveTimer);
+    self.keepAliveTimer = setTimeout(() => {
+      // Close the service worker after a certain period of inactivity
+      self.registration.unregister();
+    }, 30000); // Adjust the timeout duration as needed (currently set to 30 seconds)
+  }
 });
